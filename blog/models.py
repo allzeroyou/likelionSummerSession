ce2fb220490a65b6716ff0d9a76d1fb80b3a8c8a
allzero <0fcoursewhynot@duksung.ac.kr>
2021-07-05 21:33:27 +0900

    멋사 여름세션 첫번째 과제

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model): #상속받음: 상속받을 부모 클래스에 있는 모든 메소드, 속성 쓸 수 있음 따라서 따로 id 작성하지 않아도
+  title=models.CharField(max_length=200)
+  writer=models.CharField(max_length=100)
+  pub_date =models.DateTimeField()
+  body=models.TextField() #제한없음
+
+  def __str__(self):
+    return self.title
+
+  def summary(self):
+    return self.body[:100]
+    
